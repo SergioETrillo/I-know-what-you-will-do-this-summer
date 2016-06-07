63c6ee7aec09fb21f14380ccec64e44a7ab3127a
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-06-07 19:54:52 +0100

    WIP bug twice submit

--- app/js/app.js
+++ app/js/app.js
@@ -5,14 +5,8 @@ summerApp.config(function($routeProvider,$locationProvider){
     $routeProvider
     .when('/', {
         templateUrl: 'templates/main.html',
-        controller: 'SummaryAppController'
-    })
-    .when('/showArticle/:articleId', {
-        templateUrl: 'templates/show-article.html',
-        controller: 'SummaryAppController',
-        article_data: ''
-      })
-      .otherwise({redirectTo: '/'});
+        controller: 'summerController'
+    });
 
     $locationProvider.html5Mode(true);
 
--- app/js/controllers/summerController.js
+++ app/js/controllers/summerController.js
@@ -1,17 +1,23 @@
-angular.module("summerApp").controller("summerController", ["$env", "$http", function($env, $http) {
+angular.module("summerApp").controller("summerController", ["$env", "$http", "$route", function($env, $http, $route) {
   var self = this;
 
   self.buildQuery = function(){
-    params['location'] = self.location;
-    params['term'] = self.term;
+    params.oauth_timestamp = new Date().getTime();
+    console.log("params timestamp",params.oauth_timestamp);
+    params.location = self.location;
+    params.cc = self.cc;
+    console.log("country is:",params.cc);
+    params.term = self.term;
     var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret);
-    params['oauth_signature'] = signature;
+    params.oauth_signature = signature;
     console.log(params);
-    $http.jsonp(url, {params: params}).success(function(response){
-      self.result = response;
-      console.log(self.result);
+    $http.jsonp(url, {params: params}, {cache: false}).success(function(response){
+
+      self.results = response.businesses;
+      console.log(self.results);
     });
-  }
+    // self.reloadRoute();
+  };
 
   function randomString(length, chars) {
     var result = '';
@@ -25,8 +31,9 @@ angular.module("summerApp").controller("summerController", ["$env", "$http", fun
   var params = {
     callback: 'angular.callbacks._0',
     location: "",
+    cc: "",
     oauth_consumer_key: $env.oauth_consumer_key,
-    oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+    oauth_nonce: "",
     oauth_signature_method: "HMAC-SHA1",
     oauth_timestamp: new Date().getTime(),
     oauth_token: $env.oauth_token,
@@ -36,4 +43,8 @@ angular.module("summerApp").controller("summerController", ["$env", "$http", fun
 
   var consumerSecret = $env.consumerSecret;
   var tokenSecret = $env.tokenSecret;
+
+  self.reloadRoute = function() {
+   $route.reload();
+  };
 }]);
