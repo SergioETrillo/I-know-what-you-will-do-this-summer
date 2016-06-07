0ff384140ecb864396b0f930b5fe263b2aafa58c
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-06-07 16:15:12 +0100

    add yelp oauth after herculean debugging efforts

--- app/js/controllers/summerController.js
+++ app/js/controllers/summerController.js
@@ -1,26 +1,39 @@
-angular.module("summerApp")
-  .controller("summerController", ["$env", function($env) {
-    var self = this;
+angular.module("summerApp").controller("summerController", ["$env", "$http", function($env, $http) {
+  var self = this;
 
-    self.greeting = "Hello, summers";
+  self.buildQuery = function(){
+    params['location'] = self.location;
+    params['term'] = self.term;
+    var signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret);
+    params['oauth_signature'] = signature;
+    console.log(params);
+    $http.jsonp(url, {params: params}).success(function(response){
+      self.result = response;
+      console.log(self.result);
+    });
+  }
 
-    var method = 'GET';
-    var url = 'http://api.yelp.com/v2/search';
+  function randomString(length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+      return result;
+  }
 
-    var params = {
-      callback: 'angular.callbacks._0',
-      location: 'London',
-      oauth_consumer_key: $env.oauth_consumer_key,
-      oauth_token: $env.oauth_token,
-      oauth_signature_method: "HMAC-SHA1",
-      oauth_timestamp: new Date().getTime(),
-      // oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-      term: 'food'
-    };
-    var consumerSecret = $env.consumerSecret;
-    var tokenSecret = $env.tokenSecret;
+  var method = 'GET';
+  var url = 'http://api.yelp.com/v2/search';
 
-    var signature = oauthSignature.generate("GET",url,params, consumerSecret, tokenSecret, { encodeSignature: false});
-    console.log(signature);
-  }]);
+  var params = {
+    callback: 'angular.callbacks._0',
+    location: "",
+    oauth_consumer_key: $env.oauth_consumer_key,
+    oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+    oauth_signature_method: "HMAC-SHA1",
+    oauth_timestamp: new Date().getTime(),
+    oauth_token: $env.oauth_token,
+    oauth_version: "1.0",
+    term: ""
+  };
 
+  var consumerSecret = $env.consumerSecret;
+  var tokenSecret = $env.tokenSecret;
+}]);
