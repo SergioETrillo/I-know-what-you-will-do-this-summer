9a98eb45710ba593f5c1736110f50fe2012b1869
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-06-07 14:59:59 +0100

    oauth and trying to use envs

--- app/js/controllers/summerController.js
+++ app/js/controllers/summerController.js
@@ -3,5 +3,24 @@ angular.module("summerApp")
     var self = this;
 
     self.greeting = "Hello, summers";
+
+    var method = 'GET';
+    var url = 'http://api.yelp.com/v2/search';
+
+    var params = {
+      callback: 'angular.callbacks._0',
+      location: 'London',
+      oauth_consumer_key: $env.oauth_consumer_key,
+      oauth_token: $env.oauth_token,
+      oauth_signature_method: "HMAC-SHA1",
+      oauth_timestamp: new Date().getTime(),
+      // oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+      term: 'food'
+    };
+    var consumerSecret = $env.consumerSecret;
+    var tokenSecret = $env.tokenSecret;
+
+    var signature = oauthSignature.generate("GET",url,params, consumerSecret, tokenSecret, { encodeSignature: false});
+    console.log(signature);
   }]);
 
