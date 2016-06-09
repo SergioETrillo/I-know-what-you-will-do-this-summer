1910a6ca54a223a5a6d8116d6fe2f0a66de86d1b
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-06-09 15:41:11 +0100

    update package.json - bower install oauth-signature

--- app/env.js
+++ app/env.js
@@ -2,19 +2,11 @@ angular.module( 'env', [] )
 .service( '$env', function () {
 
   return {
-    // your configurations here,
-      oauth_consumer_key: 'c2hrE-52wukvvSguu1TybQ',
-      oauth_token: 'dNyeHDUidNiKJ0ZbE8zWteOibdSwK1GR',
-      consumerSecret: 'QdK7NysKoM3hCJZJCHx1B2TtFI8',
-      tokenSecret: 'OZwYZY4z1toHfuN6W5ca2RfzgN0',
-      weather_id: 'a3d9eb01d4de82b9b8d0849ef604dbed'
-
-    // API secrets and so here
-    // call the variable like that: $env.summaryUrl
-    // you have to inject $env in the service as such
-// newsSummaryApp.service("SummaryService", [ "$env", "SummaryFactory", "$http", function( $env, SummaryFactory, $http)
-
-
+    oauth_consumer_key: '17s2Q_1tx0CkrhJfpGo05Q',
+    oauth_token: 'XKruNkvbYWVOoFb-whvEbZVLkcu8ZLVN',
+    consumerSecret: 'pqqz4v-1829C72OpU1m-BX_yjW8',
+    tokenSecret: 'TRy9scieRpHBjWwqx_8Y-oo5g2k',
+    weather_id: 'a3d9eb01d4de82b9b8d0849ef604dbed'
   };
 
 });
